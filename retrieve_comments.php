8d23775bfe3cef86fdf18ff37f679415d1147180
cisnerosa2000 <cisnerosa2000@gmail.com>
2016-01-20 13:47:53 -0600

    Comment Retrieval Done

--- retrieve_comments.php
+++ retrieve_comments.php
@@ -16,19 +16,13 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+$query = "SELECT * FROM comments WHERE parent = '${parent_id}';";
 
+echo mysqli_query($query);
+mysqli_close($conn);
 
-
-
-
-
-
-
-
-
-
-
-
-
+if (!$query) {
+	echo "Failure!";
+}
 
 ?>
